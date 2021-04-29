72055c61fe11bbdb7a51fa386e5a4db1e4e71ef7
Mauro Passerino <mpasserino@irobot.com>
2021-04-29 14:59:54 -0300

    Add stuff

--- memory_benchmark/memory_benchmark.cpp
+++ memory_benchmark/memory_benchmark.cpp
@@ -12,19 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Check https://man7.org/linux/man-pages/man2/getrusage.2.html
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <string>
+#include <iostream>
+
 #include <chrono>
 #include <memory>
-#include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 #include "benchmark_interfaces/msg/num.hpp"
 
 using namespace std::chrono_literals;
+using namespace std;
 
-/*
-   Description: <wip>
-*/
+// Adding this class (not instanciate it) don't adds memory overhead
+using Msg = benchmark_interfaces::msg::Num;
 
 class MyNode : public rclcpp::Node
 {
@@ -34,55 +38,85 @@ public:
     const rclcpp::NodeOptions & options)
   : Node(node_name, options)
   {
-    // Publisher stuff
+    // Publisher options: avoid creating a QoS event
     auto pub_options = rclcpp::PublisherOptionsWithAllocator<std::allocator<void>>();
     pub_options.use_default_callbacks = false;
+    pub_options.use_intra_process_comm = rclcpp::IntraProcessSetting::Enable;
 
-    publisher_ = create_publisher<benchmark_interfaces::msg::Num>("topic", 10, pub_options);
+    publisher_ = create_publisher<Msg>("topic", 10, pub_options);
 
     auto timer_callback =
       [this]() -> void {
+
+        // Pub user created Msg
         auto message = benchmark_interfaces::msg::Num();
         message.num = count_++;
         RCLCPP_INFO(get_logger(), "Pub: %ld", message.num);
-        publisher_->publish(message);
-      };
-    timer_ = create_wall_timer(500ms, timer_callback);
+        publisher_->publish(std::move(message));
 
-    // Subscriber stuff
+        // Pub DDS created Msg
+        std::allocator<void> allocator;
+        rclcpp::LoanedMessage<Msg> loaned_msg(*publisher_, allocator);
+        auto & loaned_msg_ref = loaned_msg.get();
+        loaned_msg_ref.num = count_++;
+        publisher_->publish(std::move(loaned_msg));
+
+      };
+
+    timer_ = create_wall_timer(1000ms, timer_callback);
+
+    // Subscriber options: avoid creating a QoS event
     auto sub_options = rclcpp::SubscriptionOptionsWithAllocator<std::allocator<void>>();
     sub_options.use_default_callbacks = false;
+    sub_options.use_intra_process_comm = rclcpp::IntraProcessSetting::Enable;
 
-    subscription_ = create_subscription<benchmark_interfaces::msg::Num>(
-      "topic",
-      10,
-      [this](benchmark_interfaces::msg::Num::SharedPtr msg) {
-        RCLCPP_INFO(get_logger(), "   Got: %ld", msg->num);
+    subscription_ = create_subscription<Msg>(
+      "topic", 10,
+      [this](Msg::SharedPtr msg) {
+        RCLCPP_INFO(get_logger(), "  Got: %ld", msg->num);
       },
       sub_options);
   }
 
 private:
+  rclcpp::Subscription<Msg>::SharedPtr subscription_;
+  rclcpp::Publisher<Msg>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<benchmark_interfaces::msg::Num>::SharedPtr publisher_;
-  rclcpp::Subscription<benchmark_interfaces::msg::Num>::SharedPtr subscription_;
   size_t count_{0};
 };
 
+
+void print_stats(std::string str)
+{
+  struct rusage self_usage;
+  getrusage(RUSAGE_SELF, &self_usage);
+  cout << str << ": mem_max_rss_KB: " << self_usage.ru_maxrss << endl;
+}
+
 int main(int argc, char * argv[])
 {
-  std::cout << "Start test\n" << std::endl;
+  // print_stats("Start test");
+
 
   // Init with disable logging (not implemented in rmw_stub_cpp)
   rclcpp::init(argc, argv, rclcpp::InitOptions().auto_initialize_logging(false));
+  print_stats("After rclcpp::init");
+#if 0
 
   rclcpp::NodeOptions node_options = rclcpp::NodeOptions();
   node_options.use_intra_process_comms(true);
   node_options.start_parameter_services(false);
   node_options.start_parameter_event_publisher(false);
 
+  print_stats("After rclcpp::NodeOptions");
+
   auto my_node = std::make_shared<MyNode>("my_node", node_options);
+
+  print_stats("After create node");
+
   auto executor = std::make_shared<rclcpp::executors::EventsExecutor>();
+
+  print_stats("After create executor");
 
   executor->add_node(my_node);
 
@@ -92,7 +126,14 @@ int main(int argc, char * argv[])
 
   std::this_thread::sleep_for(std::chrono::seconds(3));
 
+  print_stats("Before shutdown");
   rclcpp::shutdown();
 
-  std::cout << "\nEnd test" << std::endl;
+# endif
+  // std::this_thread::sleep_for(std::chrono::seconds(30));
+  // print_stats("End test");
+  int i;
+  cin >> i;
+  cout << "Enter value to continue: " << endl;
+  print_stats("Start test");
 }
